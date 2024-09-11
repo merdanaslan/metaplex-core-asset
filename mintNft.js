e4100ddb15d0b9990f712cd14ea50adabf80319c
merdan <92172198+merdanaslan@users.noreply.github.com>
2024-09-11 14:48:51 +0200

    clean

--- mintNft.js
+++
@@ -1,9 +0,0 @@
-import { createNft } from './createNft.js';
-
-console.log('Starting NFT creation process...');
-createNft()
-  .then(() => console.log('NFT creation process completed.'))
-  .catch((error) => {
-    console.error('An error occurred during NFT creation:');
-    console.error(error);
-  });
